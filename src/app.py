a6d0bf73989d588e91de47dd48e062228f477efe
marsvo1ta <euromobilco@gmail.com>
2022-10-27 15:33:53 +0300

    del st

--- src/app.py
+++ src/app.py
@@ -1,12 +1,10 @@
-import speedtest
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # st = speedtest.Speedtest()
-    # download = st.download()
     return "Hello, world! "
 
 
